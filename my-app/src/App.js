1401244f5861ad0bc7a5e3311f4378805697cbd1
jiavu <39624614+jiavu@users.noreply.github.com>
2019-03-17 10:59:51 +0100

    created React App

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Welcome</h1>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
