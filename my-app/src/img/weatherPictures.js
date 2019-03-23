3311433037980f115a4148aa9aed0f13e2d9a7a0
jiavu <39624614+jiavu@users.noreply.github.com>
2019-03-23 21:24:03 +0100

    Added a dictionary of weather pictures
    6 pictures for thunderstorm

---
+++ my-app/src/img/weatherPictures.js
@@ -0,0 +1,40 @@
+
+let weatherPictures = {
+    c : [],
+    lc : [],
+    hc : [],
+    s : [],
+    lr : [],
+    hr : [],
+    t : [
+        {
+            name : "Ryan Phillips",
+            profileURL : "https://unsplash.com/@ryphillips",
+            url : "https://images.unsplash.com/photo-1537908829143-c49accec9f80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1386&q=80"
+        },{
+            name : "Layne Lawson",
+            profileURL : "https://unsplash.com/@laynelawson",
+            url : "https://images.unsplash.com/photo-1465799522714-8eb0e6fccf73?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1473&q=80"
+        },{
+            name : "Dejan Zakic",
+            profileURL : "https://unsplash.com/@dejan_kide",
+            url : "https://images.unsplash.com/photo-1529220100082-80957b04a221?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1301&q=80"
+        },{
+            name : "Ronald Langeveld",
+            profileURL : "https://unsplash.com/@ronaldlangeveld",
+            url : "https://images.unsplash.com/photo-1516188239414-6bfa485294d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"
+        },{
+            name : "Jeremy Thomas",
+            profileURL : "https://unsplash.com/@jeremythomasphoto",
+            url : "https://images.unsplash.com/photo-1472145246862-b24cf25c4a36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"
+        },{
+            name : "Johannes Plenio",
+            profileURL : "https://unsplash.com/@jplenio",
+            url : "https://images.unsplash.com/photo-1500674425229-f692875b0ab7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+        }
+    ],
+    h : [],
+    sl : [],
+    sn : [],
+    fog : []    // 7xx
+}
